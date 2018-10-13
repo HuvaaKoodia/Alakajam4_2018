cb9f7d5778c7232a7166323840ba3a96fc1aa844
HuvaaKoodia <huvaakoodia@gmail.com>
2018-10-13 04:55:50 +0300

    Level generator fixed

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -17,48 +17,71 @@ public class LevelGenerator : MonoBehaviour
 	}
 
 	int currentYPos = 0;
+	int lastRoomType = 0;
 	LevelDatabase.RoomData lastRoomData;
 
 	public void Generate()
 	{
 		{
-			var room = LevelDatabase.I.GetRandomRoom("Base", 0);
-			GenerateNextRoom(room);
+			
+			GenerateNextRoom();
 		}
 
 		{
-			var room = LevelDatabase.I.GetRandomRoom("Base", 0);
-			GenerateNextRoom(room);
+			GenerateNextRoom();
 		}
 
 		{
-			var room = LevelDatabase.I.GetRandomRoom("Base", 0);
-			GenerateNextRoom(room);
+			GenerateNextRoom();
 		}
 	}
 
-	private void GenerateNextRoom(LevelDatabase.RoomData room)
+	private void GenerateNextRoom()
 	{
+		
+		int type = Helpers.Rand(3);
+		if (lastRoomData != null)
+		{
+			if (lastRoomType == 0)
+				type = Helpers.RandParam(1, 2); 
+			else if (lastRoomType == 1)
+				type = Helpers.RandParam(0, 2);
+			else if (lastRoomType == 2)
+				type = Helpers.RandParam(0, 1);
+		}
+		
+		var room = LevelDatabase.I.GetRandomRoom("Base", 1 << type);
+		
 		for (int i = 0; i < room.width; i++)
 		{
-			for (int j = 0; j < room.height; j++)
-			{
-				var pos = new Vector3(i, -currentYPos + j);
+			bool topCheckOn = false;
+			if (lastRoomData != null)
+				topCheckOn = lastRoomData.data[i, 0] == TileID.Empty;
 
-				if (j == 0 && lastRoomData.data[i, lastRoomData.data.GetLength(1) - 1] == TileID.None)
+			for (int j = room.height - 1; j >= 0; j--)
+			{
+				if (topCheckOn)
 				{
-					
+					if (room.data[i, j] == TileID.Wall)
+						continue;
+					else
+						topCheckOn = false;
 				}
+				
+				var pos = new Vector3(i, -currentYPos + j);
 
 				if (room.data[i, j] == TileID.Wall)
 				{
 					var tile = Instantiate(tilePrefab, pos, Quaternion.identity);
 				}
+
 			}
+
 		}
 
 		currentYPos += room.height - 1;
 		lastRoomData = room;
+		lastRoomType = type;
 	}
 	#endregion
 	#region logic
